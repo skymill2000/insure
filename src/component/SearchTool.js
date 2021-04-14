f39ffe32aa8e9b8354e3124f68214c7bc3e4d566
skymill2000 <skymill2000@gmail.com>
2021-04-14 14:45:59 +0900

    완성본 디자인

--- src/component/SearchTool.js
+++ src/component/SearchTool.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const SearchInputBlock = styled.div`
+    padding : 10px;
+`
+
+
+const InputSearch = styled.input`
+    width : 70%;
+    margin-right : 5px;
+`
+
 const SearchTool = ({changeText, clickBtn}) => {
     return (
-        <>
-            <input type="text" onChange={changeText}></input>
+        <SearchInputBlock>
+            <InputSearch type="text" onChange={changeText}></InputSearch>
             <button onClick={clickBtn}>검색</button>
-        </>
+        </SearchInputBlock>
     );
 }
 

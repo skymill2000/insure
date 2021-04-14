49096f292ee3e12b5e1b3b9fb8895029e4303b06
skymill2000 <skymill2000@gmail.com>
2021-04-14 13:21:55 +0900

    1-1 NEW API 연동하기

--- src/component/SearchTool.js
+++ src/component/SearchTool.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const SearchTool = ({}) => {
+const SearchTool = ({changeText, clickBtn}) => {
     return (
         <>
+            <input type="text" onChange={changeText}></input>
+            <button onClick={clickBtn}>검색</button>
         </>
     );
 }
--- src/pages/NewsSearch.js
+++ src/pages/NewsSearch.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Header from '../component/Header';
 import SearchTool from '../component/SearchTool';
+import SearchResult from '../component/SearchResult';
+
+//LOGIC
 
 const NewsSearch = () => {
+    const [searchText, setSearchText] = useState('');
+    const changeText = (e) =>{
+        const {value, name} = e.target;
+        console.log(value);
+        setSearchText(value);
+    }
+
+    const clickBtn = () => {
+        console.log(searchText)
+        //axios api 요청 만들기
+        //CONSOLE.LOG 요청에 대한 결과 출력하기
+    }
+
     return (
         <>
             <Header title={'뉴스검색'}></Header>
-            <SearchTool></SearchTool>
+            <SearchTool changeText={changeText} clickBtn={clickBtn}></SearchTool>
             <SearchResult></SearchResult>
         </>
     );

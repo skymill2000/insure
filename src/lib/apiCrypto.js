bec8e6a34efd201b29a582244fbe66afd753ddb7
skymill2000 <skymill2000@gmail.com>
2021-04-15 11:52:55 +0900

    hskey 알고리즘 추가

--- src/lib/apiCrypto.js
+++ src/lib/apiCrypto.js
@@ -2,13 +2,14 @@ const crypto = require('crypto');
 const appKey = "l7xxe840074288574eeabb346f15f7a3d6af";
 const timeSt = "20210218190839";
 const caesar = require('./ceasar');
-
-const hsKey = (str) =>{
-    const key = appKey + timeSt;
-    var hmac = crypto.createHmac("sha512", new Buffer.from(key, 'utf-8'));
-    var signed = hmac.update(new Buffer.from(str, 'utf-8')).digest("base64");
-    console.log(signed);
-    return signed
+module.exports = {
+    hsKey : (str) =>{
+        const key = appKey + timeSt;
+        var hmac = crypto.createHmac("sha512", new Buffer.from(key, 'utf-8'));
+        var signed = hmac.update(new Buffer.from(str, 'utf-8')).digest("base64");
+        console.log(signed);
+        return signed
+    }
 }
 console.log(caesar.encrypt(6, 'hi how are you'));
 console.log(caesar.decrypt(9, 'no nuc gxk eua'));
--- src/pages/AuthTest.js
+++ src/pages/AuthTest.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Header from '../component/Header';
 import axios from 'axios'
-
+import cryptoMy from '../lib/apiCrypto'
 //LOGIC
 const AuthTest = () => {
     const getAccessToken = () => {
@@ -29,11 +29,38 @@ const AuthTest = () => {
             // always executed
         });
     }
+
+    const getMaster = () =>{
+        const option = {
+            method: 'post',
+            url: '',
+            headers: {
+                'Authorization' : 'Bearer ' + AccessToken,
+                'timestamp' : '20210218190839',
+                'ci' : 'E4PIs45uiscs8quYnySoQXuZKwjB66mE3Wqvw9gRuWOqMON3FiLQt+U4ZV42Y1+prQWpFWWnbKomShR+O5dHlg==',
+                'appKey': 'l7xxe840074288574eeabb346f15f7a3d6af', 
+                'appSecret': 'f8449d6220b54dddb892a262409c3e07', 
+                'Content-Type': 'application/json; charset=utf-8',
+                'hsKey' : cryptoMy.hsKey(dataBody)
+            }
+        }
+        axios(option)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+    }
      return (
         <>
             <Header title={'이용기관 ACCESS Token'}></Header>
             <button onClick={getAccessToken}>토큰발급</button>
-
+            <button onClick={getAccessToken}>마스터 증권</button>
         </>
     );
 }

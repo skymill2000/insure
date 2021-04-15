c8969c1fa83ada3436ac80199d5885adc61899e1
skymill2000 <skymill2000@gmail.com>
2021-04-15 10:39:17 +0900

    토큰 요청 완료

--- src/pages/AuthTest.js
+++ src/pages/AuthTest.js
@@ -9,22 +9,19 @@ const AuthTest = () => {
         console.log(searchText)
         //axios api 요청 만들기
         //CONSOLE.LOG 요청에 대한 결과 출력하기
-
         const option = {
-            method: 'get',
-            url: 'https://newsapi.org/v2/everything',
-            params: {
-                'q': searchText,
-                'from':'2021-04-13',
-                'sortBy': 'publishedAt',
-                'apiKey': 'c97361fe1fb44aad85885cc1a63de7fe'            
+            method: 'post',
+            url: 'https://dev-gateway.dbins.co.kr:8903/v1.0/cert/oauth2.0/clientCredential',
+            headers: {
+                'appKey': 'l7xxe840074288574eeabb346f15f7a3d6af', 
+                'appSecret': 'f8449d6220b54dddb892a262409c3e07', 
+                'Content-Type': 'application/json; charset=utf-8'
             }
         }
 
         axios(option)
         .then(function (response) {
-            console.log(response.data.articles);
-            setNews(response.data.articles);
+            console.log(response.data);
         })
         .catch(function (error) {
             // handle error
@@ -37,7 +34,7 @@ const AuthTest = () => {
      return (
         <>
             <Header title={'이용기관 ACCESS Token'}></Header>
-            <button onClick={clickBtn}>토큰발급</button>
+            <button onClick={getAccessToken}>토큰발급</button>
 
         </>
     );
